a197539a5be170bec30fb3cb6e9f943136577242
mugerw96 <mugerwaobadiah96@gmail.com>
2022-05-06 00:27:29 -0700

    init(2)

--- app/src/main/java/com/mugerwa/fireauth/LoginActivity.kt
+++ app/src/main/java/com/mugerwa/fireauth/LoginActivity.kt
@@ -3,10 +3,8 @@ package com.mugerwa.fireauth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
+import androidx.core.view.isVisible
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -27,14 +25,19 @@ class LoginActivity : AppCompatActivity() {
         auth = Firebase.auth
 
 //        binding
-        loginEmail=findViewById(R.id.loginEmail)
-        loginPassword=findViewById(R.id.loginPassword)
+        loginEmail=findViewById(R.id.registerEmail)
+        loginPassword=findViewById(R.id.registerPassword)
         loginButton=findViewById(R.id.registerBtn)
 //        actual values
 
         loginButton.setOnClickListener {
+            loginButton.isEnabled=false
+            var loginProgressBar=findViewById<ProgressBar>(R.id.progressBarLogin)
+            loginProgressBar.isVisible=true
+
             val email=loginEmail.text.toString().trim()
             val password=loginPassword.text.toString().trim()
+
             auth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
@@ -42,6 +45,7 @@ class LoginActivity : AppCompatActivity() {
                         val intent=Intent(this,MainActivity::class.java)
                         startActivity(intent)
                         finish()
+                        loginButton.isEnabled=true
                     } else {
                         // If sign in fails, display a message to the user.
                         Toast.makeText(this, "Authentication failed.",
--- app/src/main/java/com/mugerwa/fireauth/RegisterActivity.kt
+++ app/src/main/java/com/mugerwa/fireauth/RegisterActivity.kt
@@ -2,11 +2,9 @@ package com.mugerwa.fireauth
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -18,13 +16,16 @@ class RegisterActivity : AppCompatActivity() {
       private var mFirebaseAnalytics: FirebaseAnalytics? = null
 //    loginlink
     private lateinit var loginLink:TextView
-
-
+//    email
+    private lateinit var email:EditText
+    private lateinit var password:EditText
     private  lateinit var registerButton:Button
     private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
         // Initialize Firebase Auth
         auth = Firebase.auth
         // Obtain the FirebaseAnalytics instance.
@@ -33,6 +34,8 @@ class RegisterActivity : AppCompatActivity() {
 
 //        binding
         loginLink=findViewById(R.id.registerLink)
+        email=findViewById(R.id.registerEmail)
+        password=findViewById(R.id.registerPassword)
         registerButton=findViewById(R.id.registerBtn)
 
 
@@ -40,23 +43,23 @@ class RegisterActivity : AppCompatActivity() {
 
 //        register button click
         registerButton.setOnClickListener {
+            registerButton.isEnabled=false
+           val progress=findViewById<ProgressBar>(R.id.progressBar2)
+            progress.isVisible=true
             // actual values,we use trim to remove all the white spaces from the input fields
-            val email=findViewById<EditText>(R.id.loginEmail).text.toString().trim()
-            val password=findViewById<EditText>(R.id.loginPassword).text.toString().trim()
+            val email=email.text.toString().trim()
+            val password=password.text.toString().trim()
 
             auth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
-
                         // Sign in success, update UI with the signed-in user's information
-//                            when successfully registered go to the  Home apge
-                       val intent=Intent(this,MainActivity::class.java)
-                        startActivity(intent)
-                        finish()
+                        startActivity(Intent(this,MainActivity::class.java))
+                        registerButton.isEnabled=true
+                        Toast.makeText(baseContext, "Registered successfully.",Toast.LENGTH_SHORT).show()
 
-                    } else {
+                    }else {
                         // If sign in fails, display a message to the user.
-
                         Toast.makeText(baseContext, "Authentication failed.",Toast.LENGTH_SHORT).show()
 
                     }
